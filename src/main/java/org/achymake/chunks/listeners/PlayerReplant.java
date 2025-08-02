fe8dde44effbf301c2b5b9a5ce1e869dbde8f329
AchyMake <achymake@gmail.com>
2025-08-02 14:37:59 +0200

    Carry and Replant patched

--- src/main/java/org/achymake/chunks/listeners/PlayerReplant.java
+++ src/main/java/org/achymake/chunks/listeners/PlayerReplant.java
@@ -27,7 +27,7 @@ public class PlayerReplant implements Listener {
     }
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerReplant(PlayerReplantEvent event) {
-        var chunk = event.getBlock().getChunk();
+        var chunk = event.getClickedBlock().getChunk();
         if (!getChunkHandler().isClaimed(chunk))return;
         var player = event.getPlayer();
         if (getChunkHandler().hasAccess(chunk, player))return;
